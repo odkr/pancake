9eaf33a475ab3b1d01233f8d9e59fe853da3b07d
Odin Kroeger <odkr@users.noreply.github.com>
2022-04-11 19:24:18 +0200

    chore: bumped up version number.

--- pancake.lua
+++ pancake.lua
@@ -22,7 +22,7 @@
 -- @author Odin Kroeger
 -- @copyright 2022 Odin Kroeger
 -- @license MIT
--- @release 1.0.0b14
+-- @release 1.0.0b15
 
 -- Initialisation
 -- --------------
@@ -87,7 +87,7 @@ local includes = pandoc.List.includes
 -- @section
 
 --- *Pancake*'s version.
-VERSION = '1.0.0b14'
+VERSION = '1.0.0b15'
 
 --- System
 -- @section
